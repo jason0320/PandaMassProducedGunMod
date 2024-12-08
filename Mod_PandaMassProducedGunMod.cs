9f0c13c221d0d9f7065a7335028b70699b8ca1f1
jason0320 <jasonnosaj0320@gmail.com>
2024-12-08 17:41:07 +0800

    加入專案檔案。

---
+++ Mod_PandaMassProducedGunMod.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace PandaMassProducedGunMod
+{
+    [BepInPlugin("panda.massproducedgun.mod", "Panda's Mass Produced Gun Mod", "1.0.0.0")]
+    public class Mod_PandaMassProducedGunMod : BaseUnityPlugin
+    {
+        public void OnStartCore()
+        {
+            string pathToExcelFile = Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location) + "/MassProducedGun.xlsx";
+            SourceManager sources = Core.Instance.sources;
+            ModUtil.ImportExcel(pathToExcelFile, "Thing", sources.things);
+        }
+    }
+}
